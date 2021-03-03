6fb0d6b4baa24d8b79b766bdec9dc0d69d3bcf35
workJopewer <arbuztatiana@gmail.com>
2021-03-03 19:18:42 +0200

    add component form

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <form action="">
+            <div className="form-group">
+                <input type="text"
+                       className="form-control"
+                       placeholder="Внесите новую запись"/>
+            </div>
+        </form>
+    )
+}
+
+export default Form;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Form from "../components/Form";
 
 export const Home = () => {
 
+
+    return (
+        <div className='wrapper'>
+            <Form/>
+
+
+        </div>
+    )
 };
 
 export default Home;
