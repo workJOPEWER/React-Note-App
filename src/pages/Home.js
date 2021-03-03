6d281dd0748f2ae358d8c36668c0304edb8dabac
workJopewer <arbuztatiana@gmail.com>
2021-03-03 19:20:13 +0200

    add component notes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Form from "../components/Form";
+import Notes from "../components/Notes";
 
 export const Home = () => {
 
-
+    const notes = new Array(3)
+        .fill('')
+        .map((_, i) => ({id: i, title: `Note ${i + 1}`}));
     return (
         <div className='wrapper'>
             <Form/>
 
+            <hr/>
 
+            <Notes notes={notes}/>
         </div>
     )
 };
