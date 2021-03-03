a189d9984700c4c4040ac8e74f1e3acf850e3c5e
workJopewer <arbuztatiana@gmail.com>
2021-03-03 19:12:43 +0200

    add page with content

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <h1 className="display-4">Блокнот для записей</h1>
+                <p className="lead">Теперь стало проще вести свои дела. <br/>
+                    Анимационный блокнот на React -отличный помошник!</p>
+            </div>
+        </div>
+    )
+}
+export default About;
